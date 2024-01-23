b37b08056262c62b8742c79b5f8ebd151a365e8f
manePournima <manepournima7@gmail.com>
2024-01-23 13:16:58 +0530

    pushing code

---
+++ src/array_project/Arrayproject.java
@@ -0,0 +1,56 @@
+package array_project;
+
+public class Arrayproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a[][]= new int[3][3];
+		
+		int num=0;
+		
+		
+		
+		for (int i=0;i<3;i++) {
+			
+			for (int j=0;j<3;j++) {
+				
+				a[i][j]= num;
+				++num;
+				
+			}
+			
+		}
+		
+			for (int i=0;i<3;i++) {
+				
+				for(int j=0;j<3;j++) {
+					
+				
+				System.out.println (a[i][j] +  " " );
+				
+			}
+			
+			}
+			
+			
+			System.out.println();
+			}
+}
+		    
+	    
+	
+	
+	
+
+				
+				
+				
+				
+				
+				
+				
+
+
+
+
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module array_project {
+}
